e6997d00fd2b9437d9c7d0a6d08559345ab57956
Marcos Vinicius <marcosandradesteam@gmail.com>
2025-11-01 14:34:05 -0300

    ajustando env

--- class/config.php
+++ class/config.php
@@ -1,10 +1,10 @@
 <?php
 
 // 1. Carrega o autoloader do Composer (para ter acesso à biblioteca dotenv)
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // 2. Carrega o arquivo .env da raiz do projeto
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 // --- Configurações Não-Sensíveis ---
@@ -27,5 +27,3 @@ define('STEAM_API_KEY', getenv('STEAM_API_KEY'));
 define('STEAM_DOMAIN_NAME', getenv('STEAM_DOMAIN_NAME') ?: '');
 define('STEAM_LOGOUT_PAGE', getenv('STEAM_LOGOUT_PAGE') ?: '');
 define('STEAM_LOGIN_PAGE', getenv('STEAM_LOGIN_PAGE') ?: '');
-
-?>
